db92dca7f4f881f8a583a505f4fb666a936f1dcc
Alberto <info@albertojaviergarcia.com>
2020-08-24 20:10:08 +0200

    First commit. Using Entity class Product to get products

---
+++ springboot-servicio-productos/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+insert into productos (nombre, precio, create_at) values ('Samsung SSD 128GB', 100, NOW());
+insert into productos (nombre, precio, create_at) values ('Samsung SSD 256GB', 155.33, NOW());
+insert into productos (nombre, precio, create_at) values ('Samsung SSD 512GB', 200, NOW());
+insert into productos (nombre, precio, create_at) values ('WD SSD 1TB', 300, NOW());
